27bf99997d13d61d15f8903a4d79b9eed726d94d
baltikaa9 <sergey4016@mail.ru>
2024-01-10 14:31:44 +0300

    sending message when the song starts playing

--- cogs/music.py
+++ cogs/music.py
@@ -3,7 +3,7 @@ from dataclasses import dataclass
 
 import disnake
 from disnake.ext import commands
-from disnake import ApplicationCommandInteraction, VoiceClient, AudioSource, Member, VoiceState
+from disnake import ApplicationCommandInteraction, VoiceClient, AudioSource, Member, VoiceState, Message
 from yt_dlp import YoutubeDL, DownloadError
 
 from config import YDL_OPTIONS, FFMPEG_OPTIONS
@@ -23,19 +23,15 @@ class MusicCog(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot: commands.Bot = bot
 
-        # self.is_playing: bool = False
-        # self.is_paused: bool = False
-
         self.music_queue: list[MusicInfo] = []
         self.vc: VoiceClient | None = None
+        self.player_message: Message | None = None
 
     @commands.Cog.listener()
     async def on_voice_state_update(self, member: Member, before: VoiceState, after: VoiceState):
         if self.vc and before.channel and before.channel.id == self.vc.channel.id and len(before.channel.members) == 1:
             self.music_queue = []
             await self.vc.disconnect()
-            # self.is_playing = False
-            # self.is_paused = False
 
     @commands.slash_command(description='Add a song from URL or search to queue')
     async def play(self, inter: ApplicationCommandInteraction,
@@ -63,17 +59,13 @@ class MusicCog(commands.Cog):
         if not inter.author.voice:
             await inter.send(f'{inter.author.mention}, connect to a voice channel', ephemeral=True)
             return
-        # elif self.vc and self.vc.is_paused():
-        #     self.vc.resume()
-        # self.is_paused = False
 
         await inter.response.defer()
-
         music = self.ydl_search(query)
+
         if music is None:
             await inter.edit_original_response('Music not found. Try a different url')
             return
-        # self.music_queue.append(music)
 
         await inter.edit_original_response(
             f'{inter.author.mention} | Added `{music.title}` by `{music.author}` in queue')
@@ -83,7 +75,6 @@ class MusicCog(commands.Cog):
 
     @commands.slash_command(description='Pause/resume the currently playing song')
     async def pause(self, inter: ApplicationCommandInteraction):
-        # if self.vc and self.vc.is_playing():
         if not self.vc:
             await inter.send("There's no music currently playing, add some music with the `/play` command",
                              ephemeral=True)
@@ -91,23 +82,16 @@ class MusicCog(commands.Cog):
 
         if not self.vc.is_paused():
             self.vc.pause()
-            # self.is_paused = True
             await inter.send('Current music has been paused')
         else:
             self.vc.resume()
-            # self.is_paused = False
             await inter.send('Current music has been resumed')
-        # else:
-        #     await inter.send("There's no music currently playing, add some music with the `/play` command",
-        #                      ephemeral=True)
 
     @commands.slash_command(description='Stop the currently playing song')
     async def stop(self, inter: ApplicationCommandInteraction):
         if self.vc:
             self.music_queue = []
             await self.vc.disconnect()
-            # self.is_playing = False
-            # self.is_paused = False
             await inter.send('Bot has been disconnected', ephemeral=True)
         else:
             await inter.send('Bot already disconnected', ephemeral=True)
@@ -117,8 +101,7 @@ class MusicCog(commands.Cog):
         if self.vc and self.vc.is_playing():
             self.vc.stop()
             if self.music_queue:
-                await inter.send('Current music has been skipped. '
-                                 f'Now playing `{self.music_queue[0].title}` by `{self.music_queue[0].author}`')
+                await inter.send('Current music has been skipped')
             else:
                 await inter.send('Current music has been skipped. Queue is empty')
         else:
@@ -135,6 +118,10 @@ class MusicCog(commands.Cog):
             await inter.send('Queue is empty')
 
     async def play_music(self, inter: ApplicationCommandInteraction) -> None:
+        if self.player_message:
+            await self.player_message.delete()
+            self.player_message = None
+
         if self.music_queue:
             music = self.music_queue.pop(0)
 
@@ -144,14 +131,12 @@ class MusicCog(commands.Cog):
                 **FFMPEG_OPTIONS,
             )
 
-            embed = disnake.Embed(title='Now playing', description=f'`{music.title}` by `{music.author}`', color=disnake.Color.purple())
-            await inter.send(embed=embed)
+            embed = disnake.Embed(title='Now playing', description=f'`{music.title}` by `{music.author}`',
+                                  color=disnake.Color.purple())
+            self.player_message = await inter.channel.send(embed=embed)
 
-            self.vc.play(audio_src, after=lambda e: asyncio.run_coroutine_threadsafe(self.play_music(inter), self.bot.loop))
-
-        #     self.is_playing = True
-        # else:
-        #     self.is_playing = False
+            self.vc.play(audio_src,
+                         after=lambda e: asyncio.run_coroutine_threadsafe(self.play_music(inter), self.bot.loop))
 
     async def connect_to_voice_channel(self, inter: ApplicationCommandInteraction) -> None:
         if not self.vc or not self.vc.is_connected():
@@ -164,19 +149,6 @@ class MusicCog(commands.Cog):
             await self.vc.move_to(inter.author.voice.channel)
         else:
             return
-
-    # def play_next(self, error=None):
-    #     if self.music_queue:
-    #         audio_src: AudioSource = disnake.FFmpegPCMAudio(
-    #             executable='ffmpeg/ffmpeg.exe',
-    #             source=self.music_queue.pop(0).url,
-    #             **FFMPEGOPTIONS,
-    #         )
-    #
-    #         self.vc.play(audio_src, after=self.play_next)
-    #     self.is_playing = True
-    # else:
-    #     self.is_playing = False
 
     @staticmethod
     def ydl_search(query: str) -> MusicInfo | None:
